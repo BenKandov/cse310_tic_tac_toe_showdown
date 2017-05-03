414cca3f88b66f027c6fea72b545066a623ce689
Manan <manan.shine@gmail.com>
2017-05-03 16:19:16 -0400

    Made in china

--- client.py
+++ client.py
@@ -1,86 +1,126 @@
-from sys import argv
+from sys import argv, stdin
 from socket import *
+import select
 
-# command line args
-hostname = argv[1]
-port = int(argv[2])
 
-# boolean for exiting; starts as True
+# boolean for exiting; start as True
 running = True
 
 def help_cmd(exec_args):
     print('This is the help menu')
 
 def login(exec_args):
-    if not exec_args['c_args']:
+    if len(exec_args['c_args']) < 1:
         print('You must choose a name to login as')
+    elif len(exec_args['c_args'][0]) > 50:
+        print('Your username can only be 50 characters at most')
     else:
         username = exec_args['c_args'][0]
-        exec_args['socket'].send(username.encode())
-        ack = exec_args['socket'].recv()
-        ack = ack.decode()
-        
-        if ack == 'y':
-            # if server responds with 'y', it worked
-            print('Your username is now %s' % (username,))
-        elif ack == 't':
-            # if server responsds with 't', that username is taken already
-            print('That username is already taken')
-        else:
-            # the protocol is not being followed if you end up here
-            print('Could not decode server response')
+        protocol_cmd = 'LOGIN %s\r\n' % (username,)
+        exec_args['socket'].send(protocol_cmd.encode())
 
 def place(exec_args):
-    if not exec_args['c_args']:
-        print('Which cell you like to make your move in?')
+    if not len(exec_args['c_args']) < 1:
+        print('Please specify which cell you like to make your move in')
     else:
-        cell = exec_args['c_args']
-        exec_args['socket'].send(str(cell).encode()) # sending cell as a string
-        ack = exec_args['socket'].recv()
-        ack = ack.decode()
-
-        if ack == 'n':
-            # server sent back an error, illegal move
-            print('Illegal move')
-        else:
-            # server sent back a board you can print
-            print(ack)
+        cell = exec_args['c_args'][0]
+        protocol_cmd = 'PLACE %s\r\n' % (cell,)
+        exec_args['socket'].send(protocol_cmd.encode())
 
 def exit_game(exec_args):
     global running
 
-    running = False
+    protocol_cmd = 'EXIT\r\n'
+    exec_args['socket'].send(protocol_cmd.encode())
     exec_args['socket'].close()
-    print('Thanks for playing')
+    running = False
 
-# dictionary of function pointers to make cmd parsing easy
+def games(exec_args):
+    protocol_cmd = 'GAMES\r\n'
+
+def who(exec_args):
+    protocol_cmd = 'WHO\r\n'
+    exec_args['socket'].send(protocol_cmd.encode())
+
+def play(exec_args):
+    if not len(exec_args['c_args']) < 1:
+        print('Specify who you would like to play as')
+    else:
+        opponent = exec_args['c_args'][0]
+        protocol_cmd = 'PLAY %s\r\n' % (opponent,)
+        exec_args['socket'].send(protocol_cmd.encode())
+
+def handle_msg(msg):
+    # msg is an array of string
+    if msg[0] == '200 OHW':
+        print('These players are on right now:')
+        to_print = msg[1].split(',')
+        for x in to_print:
+            print(x)
+    elif msg[0] == '200 SEMAG':
+        print('These are the current games:')
+        to_print = msg[1].split(',')
+        for x in to_print:
+            print(x)
+    else:
+        print(msg[1])
+
 commands = {
-        'help': help_cmd,
-        'login': login,
-        'place': place,
-        'exit': exit_game
+    'help': help_cmd,
+    'login': login,
+    'place': place,
+    'exit': exit_game
 }
 
 if __name__ == '__main__':
-    if not argv[1] and not arg[2]:
-        print('This program needs a hostname and port')
-    else: 
-        client_soc = socket.socket(AF_INET, SOCK_STREAM)
+    if len(argv) < 2:
+        print('This program requires a hostname and port as an argument')
+    elif len(argv) > 3 and argv[3] != 'a':
+        print("You can use 'a' to specify the automatch flag")
+    else:
+        # command line args
+        hostname = argv[1]
+        port = int(argv[2])
+
+        client_soc = socket(AF_INET, SOCK_STREAM)
         client_soc.connect( (hostname, port) )
-        
+
+        # this is what select chooses from
+        read_list = [ stdin, client_soc ]
+
+        if argv[3]:
+            # if the being run with the automatch flag, then send a quick AUTOMATCH req
+            client_soc.send('AUTOLOGIN\r\n'.encode())
+        else:
+            # client can make more advanced requests if not being automatched
+            commands['games'] = games
+            commands['play'] = play
+            commands['who'] = who
+
         while running:
-            cmd = input('Type a command (or help if you are not sure): ')
-            sanitized = cmd.strip() # strip beginning & ending whitespace
-            parse_list = sanitized.split(' ', 1) # split into func name and rest of args
+            print('Type a command (or help if you are not sure): ')
+            # select will return 3 lists of active fds, you care about 'readable'
+            readable, writeable, exceptional = select.select(read_list, [], [])           
+            for r in readable:
+                if r == stdin:
+                    # the user typed a command, handle it
+                    cmd = stdin.readline()
+                    sanatized = cmd.rstrip('\n').strip() # strip the beginning & ending whitespace
+                    parse_list = sanatized.split(' ', 1) # split into fun name and rest of args
 
-            exec_func = parse_list[0] # take out the actual function call
-            exec_args = { 'socket': client_soc} # always gets passed to the command functions
+                    exec_func = parse_list[0] # take out the actual function call
+                    exec_args = { 'socket': client_soc } # always gets passed to the command functions
 
-            if len(parse_list) > 1:
-                # if a command takes args, then the functions now have access to them
-                exec_args['c_args'] = list(filter(lambda x: x != '', parse_list[1].split(' '))) 
+                    if len(parse_list) > 1:
+                        # if a command takes args, then the functions now have access to them
+                        exec_args['c_args'] = list(filter(lambda x: x != ''. parse_list[1].split(' ')))
 
-            if exec_func not in commands:
-                print('Not a valid command')
-            else:
-                commands[exec_func](exec_args)
+                    if exec_func not in commands:
+                        print('Not a valid command')
+                    else:
+                        commands[exec_func](exec_args)
+                else:
+                    # you can read from the socket 
+                    message = client_soc.recv(1024).decode()
+                    message = message.split('\n')
+                    handle_msg(message)
